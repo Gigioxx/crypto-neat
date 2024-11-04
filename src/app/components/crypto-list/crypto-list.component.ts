57cb0d9c38fc05ae5e4d0f52a92a4fa9881cfed8
Gigiox <guillermo.casanova.b@gmail.com>
2024-11-04 02:27:11 -0300

    chore: fix

--- src/app/components/crypto-list/crypto-list.component.ts
+++ src/app/components/crypto-list/crypto-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CryptoResponseArray } from '../../interfaces/crypto.interfaces';
+import { CryptoCardComponent } from "../crypto-card/crypto-card.component";
 
 @Component({
   selector: 'app-crypto-list',
   standalone: true,
-  imports: [],
+  imports: [CryptoCardComponent],
   templateUrl: './crypto-list.component.html',
 })
 export class CryptoListComponent {
-  @Input() CryptoData: CryptoResponseArray = [];
+  @Input() cryptoData: CryptoResponseArray = [];
 
 }
