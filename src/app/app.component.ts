9d557e44df10c64dad790f729ae7c6967cd9bbfc
Gigiox <guillermo.casanova.b@gmail.com>
2024-11-04 01:51:31 -0300

    chore: clean main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'crypto-neat';
