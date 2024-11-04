9f65af7f6f6e008cfb22a75690faf04509328f5c
Gigiox <guillermo.casanova.b@gmail.com>
2024-11-04 01:49:23 -0300

    feat: create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BuyCryptoPageComponent } from './pages/buy-crypto/buy-crypto-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'buy',
+    component: BuyCryptoPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: () => {
+      // const authService = inject(AuthService)
+
+      return 'home';
+    },
+  },
+];
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+// src/app/pages/home-page/home-page.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {
+
+}
