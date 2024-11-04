ff8362f76bec918629567730014dae0141732a0e
Gigiox <guillermo.casanova.b@gmail.com>
2024-11-04 01:51:04 -0300

    chore: add colors according to design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   content: [
-    './src/**/*.{html,ts,scss}',
+    './src/**/*.{html,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#FCFDFF',
+        'text': '151F6D',
+        'primary': '#3369F0',
+        '100': 'F04E98',
+        'text-success': '#50A684',
+        'text-error': '#F9423A',
+        'text-warning': '#FF9E1B',
+      }
+    },
   },
   plugins: [],
 };
